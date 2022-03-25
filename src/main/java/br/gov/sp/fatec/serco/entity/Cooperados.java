c9ead7769a30112adc35bbc3db8de40f95ff584b
KleberNascimento3 <kleberrroger@hotmail.com>
2022-03-25 12:42:30 +0000

    Gerando os Gets and setters

--- src/main/java/br/gov/sp/fatec/serco/entity/Cooperados.java
+++ src/main/java/br/gov/sp/fatec/serco/entity/Cooperados.java
@@ -1,5 +1,7 @@
 package br.gov.sp.fatec.serco.entity;
 
+import java.sql.Date;
+
 public class Cooperados{
     private Long index_cod;
     private Long matricula;
@@ -77,4 +79,463 @@ public class Cooperados{
     private String beneficio;
     private Long dep_ir;
     private String Isqn;
+    
+    public Long getIndex_cod() {
+        return index_cod;
+    }
+    public void setIndex_cod(Long index_cod) {
+        this.index_cod = index_cod;
+    }
+    public Long getMatricula() {
+        return matricula;
+    }
+    public void setMatricula(Long matricula) {
+        this.matricula = matricula;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getNomeguerra() {
+        return nomeguerra;
+    }
+    public void setNomeguerra(String nomeguerra) {
+        this.nomeguerra = nomeguerra;
+    }
+    public String getPai() {
+        return pai;
+    }
+    public void setPai(String pai) {
+        this.pai = pai;
+    }
+    public String getMae() {
+        return mae;
+    }
+    public void setMae(String mae) {
+        this.mae = mae;
+    }
+    public String getConjuge() {
+        return conjuge;
+    }
+    public void setConjuge(String conjuge) {
+        this.conjuge = conjuge;
+    }
+    public Long getNumfilhos() {
+        return numfilhos;
+    }
+    public void setNumfilhos(Long numfilhos) {
+        this.numfilhos = numfilhos;
+    }
+    public String getLocalnasc() {
+        return localnasc;
+    }
+    public void setLocalnasc(String localnasc) {
+        this.localnasc = localnasc;
+    }
+    public String getTelefoneresidencial() {
+        return telefoneresidencial;
+    }
+    public void setTelefoneresidencial(String telefoneresidencial) {
+        this.telefoneresidencial = telefoneresidencial;
+    }
+    public String getTelefonecelular() {
+        return telefonecelular;
+    }
+    public void setTelefonecelular(String telefonecelular) {
+        this.telefonecelular = telefonecelular;
+    }
+    public String getTelefonecomercial() {
+        return telefonecomercial;
+    }
+    public void setTelefonecomercial(String telefonecomercial) {
+        this.telefonecomercial = telefonecomercial;
+    }
+    public Date getDatanascimento() {
+        return datanascimento;
+    }
+    public void setDatanascimento(Date datanascimento) {
+        this.datanascimento = datanascimento;
+    }
+    public Date getDatacadastro() {
+        return datacadastro;
+    }
+    public void setDatacadastro(Date datacadastro) {
+        this.datacadastro = datacadastro;
+    }
+    public Date getDataadmissao() {
+        return dataadmissao;
+    }
+    public void setDataadmissao(Date dataadmissao) {
+        this.dataadmissao = dataadmissao;
+    }
+    public Date getDatadesligamento() {
+        return datadesligamento;
+    }
+    public void setDatadesligamento(Date datadesligamento) {
+        this.datadesligamento = datadesligamento;
+    }
+    public String getMotivo() {
+        return motivo;
+    }
+    public void setMotivo(String motivo) {
+        this.motivo = motivo;
+    }
+    public String getCooperado() {
+        return cooperado;
+    }
+    public void setCooperado(String cooperado) {
+        this.cooperado = cooperado;
+    }
+    public Date getDataadmissibilidade() {
+        return dataadmissibilidade;
+    }
+    public void setDataadmissibilidade(Date dataadmissibilidade) {
+        this.dataadmissibilidade = dataadmissibilidade;
+    }
+    public String getTestepsicologico() {
+        return testepsicologico;
+    }
+    public void setTestepsicologico(String testepsicologico) {
+        this.testepsicologico = testepsicologico;
+    }
+    public String getTestetecnico() {
+        return testetecnico;
+    }
+    public void setTestetecnico(String testetecnico) {
+        this.testetecnico = testetecnico;
+    }
+    public String getInformacoes() {
+        return informacoes;
+    }
+    public void setInformacoes(String informacoes) {
+        this.informacoes = informacoes;
+    }
+    public String getRestricao() {
+        return restricao;
+    }
+    public void setRestricao(String restricao) {
+        this.restricao = restricao;
+    }
+    public String getAnotacoes() {
+        return anotacoes;
+    }
+    public void setAnotacoes(String anotacoes) {
+        this.anotacoes = anotacoes;
+    }
+    public String getEndereco() {
+        return endereco;
+    }
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    public String getBairro() {
+        return bairro;
+    }
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+    public String getCidade() {
+        return cidade;
+    }
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+    public String getEstado() {
+        return estado;
+    }
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    public String getCep() {
+        return cep;
+    }
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+    public String getPais() {
+        return pais;
+    }
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+    public Date getDatatestepsico() {
+        return datatestepsico;
+    }
+    public void setDatatestepsico(Date datatestepsico) {
+        this.datatestepsico = datatestepsico;
+    }
+    public Date getDatatestetecnico() {
+        return datatestetecnico;
+    }
+    public void setDatatestetecnico(Date datatestetecnico) {
+        this.datatestetecnico = datatestetecnico;
+    }
+    public String getFoto() {
+        return foto;
+    }
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+    public String getRg() {
+        return rg;
+    }
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+    public String getRgoem() {
+        return rgoem;
+    }
+    public void setRgoem(String rgoem) {
+        this.rgoem = rgoem;
+    }
+    public Date getRgemis() {
+        return rgemis;
+    }
+    public void setRgemis(Date rgemis) {
+        this.rgemis = rgemis;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    public String getInss() {
+        return inss;
+    }
+    public void setInss(String inss) {
+        this.inss = inss;
+    }
+    public String getEscolaridade() {
+        return escolaridade;
+    }
+    public void setEscolaridade(String escolaridade) {
+        this.escolaridade = escolaridade;
+    }
+    public String getEstadoCivil() {
+        return estadoCivil;
+    }
+    public void setEstadoCivil(String estadoCivil) {
+        this.estadoCivil = estadoCivil;
+    }
+    public Long getArquivo() {
+        return arquivo;
+    }
+    public void setArquivo(Long arquivo) {
+        this.arquivo = arquivo;
+    }
+    public Long getGaveta() {
+        return gaveta;
+    }
+    public void setGaveta(Long gaveta) {
+        this.gaveta = gaveta;
+    }
+    public Long getPasta() {
+        return pasta;
+    }
+    public void setPasta(Long pasta) {
+        this.pasta = pasta;
+    }
+    public String getSexo() {
+        return sexo;
+    }
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+    public String getSel() {
+        return sel;
+    }
+    public void setSel(String sel) {
+        this.sel = sel;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public Long getIndicacao() {
+        return indicacao;
+    }
+    public void setIndicacao(Long indicacao) {
+        this.indicacao = indicacao;
+    }
+    public Long getValor_pgbl() {
+        return valor_pgbl;
+    }
+    public void setValor_pgbl(Long valor_pgbl) {
+        this.valor_pgbl = valor_pgbl;
+    }
+    public Long getPerc_pgbl() {
+        return perc_pgbl;
+    }
+    public void setPerc_pgbl(Long perc_pgbl) {
+        this.perc_pgbl = perc_pgbl;
+    }
+    public Long getNrdep() {
+        return nrdep;
+    }
+    public void setNrdep(Long nrdep) {
+        this.nrdep = nrdep;
+    }
+    public String getAniversario() {
+        return aniversario;
+    }
+    public void setAniversario(String aniversario) {
+        this.aniversario = aniversario;
+    }
+    public Long getIdade() {
+        return idade;
+    }
+    public void setIdade(Long idade) {
+        this.idade = idade;
+    }
+    public Long getOcorrencias() {
+        return ocorrencias;
+    }
+    public void setOcorrencias(Long ocorrencias) {
+        this.ocorrencias = ocorrencias;
+    }
+    public String getPassaporte() {
+        return passaporte;
+    }
+    public void setPassaporte(String passaporte) {
+        this.passaporte = passaporte;
+    }
+    public Date getDataexpedicao() {
+        return dataexpedicao;
+    }
+    public void setDataexpedicao(Date dataexpedicao) {
+        this.dataexpedicao = dataexpedicao;
+    }
+    public Date getDt_vencto_pass() {
+        return dt_vencto_pass;
+    }
+    public void setDt_vencto_pass(Date dt_vencto_pass) {
+        this.dt_vencto_pass = dt_vencto_pass;
+    }
+    public String getOem_pass() {
+        return oem_pass;
+    }
+    public void setOem_pass(String oem_pass) {
+        this.oem_pass = oem_pass;
+    }
+    public String getNie_nr() {
+        return nie_nr;
+    }
+    public void setNie_nr(String nie_nr) {
+        this.nie_nr = nie_nr;
+    }
+    public Date getNie_vencto() {
+        return nie_vencto;
+    }
+    public void setNie_vencto(Date nie_vencto) {
+        this.nie_vencto = nie_vencto;
+    }
+    public String getEndereco2() {
+        return endereco2;
+    }
+    public void setEndereco2(String endereco2) {
+        this.endereco2 = endereco2;
+    }
+    public String getBairro2() {
+        return bairro2;
+    }
+    public void setBairro2(String bairro2) {
+        this.bairro2 = bairro2;
+    }
+    public String getCidade2() {
+        return cidade2;
+    }
+    public void setCidade2(String cidade2) {
+        this.cidade2 = cidade2;
+    }
+    public String getEstado2() {
+        return estado2;
+    }
+    public void setEstado2(String estado2) {
+        this.estado2 = estado2;
+    }
+    public String getPais2() {
+        return pais2;
+    }
+    public void setPais2(String pais2) {
+        this.pais2 = pais2;
+    }
+    public String getCep2() {
+        return cep2;
+    }
+    public void setCep2(String cep2) {
+        this.cep2 = cep2;
+    }
+    public String getTelres2() {
+        return telres2;
+    }
+    public void setTelres2(String telres2) {
+        this.telres2 = telres2;
+    }
+    public String getTelcel2() {
+        return telcel2;
+    }
+    public void setTelcel2(String telcel2) {
+        this.telcel2 = telcel2;
+    }
+    public String getTelcom2() {
+        return telcom2;
+    }
+    public void setTelcom2(String telcom2) {
+        this.telcom2 = telcom2;
+    }
+    public String getEb2() {
+        return eb2;
+    }
+    public void setEb2(String eb2) {
+        this.eb2 = eb2;
+    }
+    public Date getEb2_dtinicio() {
+        return eb2_dtinicio;
+    }
+    public void setEb2_dtinicio(Date eb2_dtinicio) {
+        this.eb2_dtinicio = eb2_dtinicio;
+    }
+    public Date getEb2_dtvencto() {
+        return eb2_dtvencto;
+    }
+    public void setEb2_dtvencto(Date eb2_dtvencto) {
+        this.eb2_dtvencto = eb2_dtvencto;
+    }
+    public String getAposentado() {
+        return aposentado;
+    }
+    public void setAposentado(String aposentado) {
+        this.aposentado = aposentado;
+    }
+    public String getBeneficio() {
+        return beneficio;
+    }
+    public void setBeneficio(String beneficio) {
+        this.beneficio = beneficio;
+    }
+    public Long getDep_ir() {
+        return dep_ir;
+    }
+    public void setDep_ir(Long dep_ir) {
+        this.dep_ir = dep_ir;
+    }
+    public String getIsqn() {
+        return Isqn;
+    }
+    public void setIsqn(String isqn) {
+        Isqn = isqn;
+    }
+
+    
 }
