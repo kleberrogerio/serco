5df2ef86dcef3d709300d70d06edeaf5b0e6b322
KleberNascimento3 <kleberrroger@hotmail.com>
2022-03-28 16:19:57 +0000

    Começando o TestaInserção

--- src/test/java/br/gov/sp/fatec/serco/SercoApplicationTests.java
+++ src/test/java/br/gov/sp/fatec/serco/SercoApplicationTests.java
@@ -1,13 +1,28 @@
 package br.gov.sp.fatec.serco;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import br.gov.sp.fatec.serco.entity.Cooperados;
+import br.gov.sp.fatec.serco.repository.CooperadoRespository;
 
 @SpringBootTest
 class SercoApplicationTests {
 
+	@Autowired
+	private CooperadoRespository cooperadoRepo;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testaInsercao(){
+		Cooperados cooperado = new Cooperados();
+		cooperado.setNome("Kleber");
+
+
 	}
 
 }
